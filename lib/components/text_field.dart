960796a204e502cb038dae8d54187bc523567c13
Maxim <bulanovich5@seznam.cz>
2023-04-26 16:10:46 +0200

    register screen - ui

--- lib/components/text_field.dart
+++ lib/components/text_field.dart
@@ -16,28 +16,29 @@ class _MyTextFieldState extends State<MyTextField> {
     double width = MediaQuery.of(context).size.width;
     return Container(
       width: width * 0.8,
-      height: 60,
+      height: 52,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.white, width: 1.5),
+        border: Border.all(color: Colors.white, width: 1.7),
         borderRadius: BorderRadius.circular(10),
         gradient: const LinearGradient(
           colors: [
-            Color.fromARGB(255, 237, 221, 255),
-            Color.fromARGB(255, 158, 187, 255),
+            Color.fromARGB(155, 172, 172, 172),
+            Color.fromARGB(174, 33, 33, 33)
           ],
         ),
       ),
       child: Center(
         child: TextFormField(
           decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: widget.hintText,
-              hintStyle: GoogleFonts.signikaNegative(
-                color: const Color.fromARGB(255, 63, 75, 187),
-                fontSize: 20,
-              ),
-              prefixIcon: widget.icon,
-              prefixIconColor: const Color.fromARGB(255, 63, 75, 187)),
+            border: InputBorder.none,
+            hintText: widget.hintText,
+            hintStyle: GoogleFonts.signikaNegative(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+            prefixIcon: widget.icon,
+            prefixIconColor: Colors.white,
+          ),
         ),
       ),
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:motoappv2/components/button.dart';
 import 'package:motoappv2/components/text_field.dart';
 import 'package:motoappv2/helpers/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,43 +21,86 @@ class _RegisterPageState extends State<RegisterPage> {
 
     return Scaffold(
       backgroundColor: MyColors.bgcolor,
-      body: SafeArea(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("lib/assets/bg.gif"), fit: BoxFit.cover),
+        ),
         child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 130),
-              Text(
-                'Register',
-                style: GoogleFonts.signikaNegative(
-                  color: Colors.white,
-                  fontSize: width * 0.11,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                Text(
+                  'Register',
+                  style: GoogleFonts.signikaNegative(
+                    color: Colors.white,
+                    fontSize: width * 0.13,
+                    fontWeight: FontWeight.w600
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'Create your account',
-                style: GoogleFonts.signikaNegative(
-                  color: Colors.white,
-                  fontSize: width * 0.04,
+
+                Text(
+                  'Create your account',
+                  style: GoogleFonts.signikaNegative(
+                    color: Colors.white,
+                    fontSize: width * 0.04,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 40),
-              //TextFields
-              const MyTextField(
-                hintText: 'Email address',
-                icon: Icon(Icons.email),
-              ),
-              const SizedBox(height: 20),
-              const MyTextField(
-                hintText: 'Password',
-                icon: Icon(Icons.password_outlined),
-              ),
-               const SizedBox(height: 20),
-              const MyTextField(
-                hintText: 'Confirm password',
-                icon: Icon(Icons.password_outlined),
-              )
-            ],
+                const SizedBox(height: 110),
+                //TextFields
+                const MyTextField(
+                  hintText: 'Email address',
+                  icon: Icon(Icons.email),
+                ),
+                const SizedBox(height: 20),
+                const MyTextField(
+                  hintText: 'Password',
+                  icon: Icon(Icons.password_outlined),
+                ),
+                const SizedBox(height: 20),
+                const MyTextField(
+                  hintText: 'Confirm password',
+                  icon: Icon(Icons.password_outlined),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child:
+                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    Text(
+                      'Forgot Password?',
+                      style: GoogleFonts.signikaNegative(
+                        color: Colors.white,
+                        fontSize: width * 0.035,
+                      ),
+                    ),
+                    SizedBox(
+                      width: width * 0.1,
+                    )
+                  ]),
+                ),
+                const SizedBox(height: 30),
+                MyButton(
+                    color: const Color.fromARGB(255, 232, 75, 75),
+                    icon: false,
+                    title: 'Sign Up',
+                    onTap: () {}),
+                const SizedBox(height: 7),
+                MyButton(
+                    color: Color.fromARGB(115, 118, 118, 118),
+                    icon: true,
+                    title: 'Continue with Google',
+                    onTap: () {}),
+                const SizedBox(height: 7),
+                Text(
+                  'Already have an account? Login',
+                  style: GoogleFonts.signikaNegative(
+                    color: Colors.white,
+                    fontSize: width * 0.035,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
